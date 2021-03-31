a9d7551be6bfea548d7d2845a0b52702e33af361
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-03-31 11:57:26 -0300

    paleta de cores

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,29 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return (
     <div>
       <div>
-        <nav></nav>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/">Quem sou eu?</Link>
+            </li>
+            <li>
+              <Link href="/">Projetos</Link>
+            </li>
+            <li>
+              <Link href="/">Tecnologias</Link>
+            </li>
+            <li>
+              <Link href="/">Entre em contato</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1>Me chamo Bryan</h1>
       <h2>e sou um desenvolvedor Front-End</h2>
     </div>
