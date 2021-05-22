6b9d7f82a2a82c7db43f46b916cc758dd7539827
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-05-22 11:01:25 -0300

    flaticon adicionado

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,17 @@ import Header from "../components/Header";
 import "../styles/global.scss";
 import styles from '../styles/app.module.scss';
 import {motion} from 'framer-motion'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <div className={styles.wrapper}>
-      <Header />
+      <Head>
+        <title>Bryan da Silva Bruzinga - Portfólio Web</title>
+        <link rel="icon" href="/flaticon.ico.png" />
+      </Head>      
       <main>
+        <Header />
         <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" variants={{
           pageInitial: {
             opacity: 0
